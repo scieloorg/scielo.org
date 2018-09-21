16e6693d63a7ac25c691e578897ab81a347d1619
Juliano Maciel <julianomfferreira@gmail.com>
2018-09-21 15:57:49 -0300

    Inclusão de classe CSS para ajustar espaçamento quando não há valores para as coleções.

--- application/views/templates/collections.php
+++ application/views/templates/collections.php
@@ -9,7 +9,7 @@ $lang_index = isset($language) ? $language : SCIELO_EN_LANG;
         <dl>
             <dt><h3><?= lang('books_list') ?></h3></dt>
             <?php foreach ($this->Collections->get_books_list() as $key => $book) : ?>
-                <dd class="scielo-books">
+                <dd class="scielo-books scielo-books-no-data">
                     <a href="http://<?= $book->domain ?>">
                         <h4><?= $book->name[$lang_index] ?></h4>
                     </a>
@@ -18,40 +18,60 @@ $lang_index = isset($language) ? $language : SCIELO_EN_LANG;
 
             <dt><h3><?= lang('journals_list') ?></h3></dt>
             <?php foreach ($this->Collections->get_journals_list() as $key => $journal) : ?>
-                 <dd class="flag-<?= $journal->code ?>">
+            <?php
+            $has_journal_count = array_key_exists('current', $journal->journal_count);
+            $has_document_count = !empty($journal->document_count);
+            $flag_no_data_css = (!$has_journal_count && !$has_document_count) ? 'flag-no-data' : null;
+            ?>
+                 <dd class="flag-<?= $journal->code ?> <?= $flag_no_data_css ?>">
                     <a href="http://<?= $journal->domain ?>">
                         <h4><?= $journal->name[$lang_index] ?></h4>
-                        <span><?php if (array_key_exists('current', $journal->journal_count)): ?><?= $journal->journal_count['current'] ?> <?= lang('journals') ?> •<?php endif; ?> <?php if(!empty($journal->document_count)):?><?= $journal->document_count ?> <?= lang('articles') ?><?php endif;?></span>
+                        <span><?php if ($has_journal_count) : ?><?= $journal->journal_count['current'] ?> <?= lang('journals') ?> •<?php endif; ?> <?php if ($has_document_count) : ?><?= $journal->document_count ?> <?= lang('articles') ?><?php endif; ?></span>
                     </a>
                 </dd>
             <?php endforeach; ?>
 
             <dt><h3><?= lang('development_list') ?></h3></dt>        
             <?php foreach ($this->Collections->get_development_list() as $key => $development) : ?>
-                <dd class="flag-<?= $development->code ?>">
+            <?php
+            $has_journal_count = array_key_exists('current', $development->journal_count);
+            $has_document_count = !empty($development->document_count);
+            $flag_no_data_css = (!$has_journal_count && !$has_document_count) ? 'flag-no-data' : null;
+            ?>
+                <dd class="flag-<?= $development->code ?> <?= $flag_no_data_css ?>">
                     <a href="http://<?= $development->domain ?>">
                         <h4><?= $development->name[$lang_index] ?></h4>
-                        <span><?php if (array_key_exists('current', $development->journal_count)): ?><?= $development->journal_count['current'] ?> <?= lang('journals') ?> •<?php endif; ?> <?php if(!empty($development->document_count)):?><?= $development->document_count ?> <?= lang('articles') ?><?php endif;?></span>
+                        <span><?php if ($has_journal_count) : ?><?= $development->journal_count['current'] ?> <?= lang('journals') ?> •<?php endif; ?> <?php if ($has_document_count) : ?><?= $development->document_count ?> <?= lang('articles') ?><?php endif; ?></span>
                     </a>
                 </dd>
             <?php endforeach; ?>
 
             <dt><h3><?= lang('discontinued_list') ?></h3></dt>
             <?php foreach ($this->Collections->get_discontinued_list() as $key => $discontinued) : ?>
-                <dd class="scielo-books">
+            <?php
+            $has_journal_count = array_key_exists('deceased', $discontinued->journal_count);
+            $has_document_count = !empty($discontinued->document_count);
+            $scielo_books_no_data_css = (!$has_journal_count && !$has_document_count) ? 'scielo-books-no-data' : null;
+            ?>
+                <dd class="scielo-books <?= $scielo_books_no_data_css ?>">
                     <a href="http://<?= $discontinued->domain ?>">
                         <h4><?= $discontinued->name[$lang_index] ?></h4>
-                        <span><?php if (array_key_exists('deceased', $discontinued->journal_count)): ?><?= $discontinued->journal_count['deceased'] ?> <?= lang('journals') ?> •<?php endif; ?> <?php if(!empty($discontinued->document_count)):?><?= $discontinued->document_count ?> <?= lang('articles') ?><?php endif;?></span>
+                        <span><?php if ($has_journal_count) : ?><?= $discontinued->journal_count['deceased'] ?> <?= lang('journals') ?> •<?php endif; ?> <?php if ($has_document_count) : ?><?= $discontinued->document_count ?> <?= lang('articles') ?><?php endif; ?></span>
                     </a>
                 </dd>
             <?php endforeach; ?>
 
             <dt><h3><?= lang('scientific_list') ?></h3></dt>
             <?php foreach ($this->Collections->get_scientific_list() as $key => $scientific) : ?>
-                <dd class="scielo-books">
+            <?php
+            $has_journal_count = array_key_exists('current', $scientific->journal_count);
+            $has_document_count = !empty($scientific->document_count);
+            $scielo_books_no_data_css = (!$has_journal_count && !$has_document_count) ? 'scielo-books-no-data' : null;
+            ?>
+                <dd class="scielo-books <?= $scielo_books_no_data_css ?>">
                     <a href="http://<?= $scientific->domain ?>">
                         <h4><?= $scientific->name[$lang_index] ?></h4>
-                        <span><?php if (array_key_exists('current', $scientific->journal_count)): ?><?= $scientific->journal_count['current'] ?> <?= lang('journals') ?> •<?php endif; ?> <?php if(!empty($scientific->document_count)):?><?= $scientific->document_count ?> <?= lang('articles') ?><?php endif;?></span>
+                        <span><?php if ($has_journal_count) : ?><?= $scientific->journal_count['current'] ?> <?= lang('journals') ?> •<?php endif; ?> <?php if ($has_document_count) : ?><?= $scientific->document_count ?> <?= lang('articles') ?><?php endif; ?></span>
                     </a>
                 </dd>
             <?php endforeach; ?>
